7fd5c8396cfc6c7fde26576f62d0ec3ca13b99b0
Koneko <67551503+koneko@users.noreply.github.com>
2022-07-17 22:25:42 +0200

    Create solars.lua

---
+++ solars.lua
@@ -0,0 +1,43 @@
+local Flip_Forward   = 10261581356
+local Flip_Backwards = 10261579909
+local Stun_Animation = 10261598046
+
+-- Special
+
+local TG_Idle        = 10261601360
+
+-- Single animations
+
+local Idle_Animation = 10261586183 -- These IDs were placed directly into the NewAnimation functions before, I've set them up this way for you because it's easier
+local Right_Slash_1  = 10261590054
+local Right_Slash_2  = 10261594388
+local Left_Slash_1   = 10261587653
+local Left_Slash_2   = 10261588835
+
+local Block_Neutral  = 10261537042
+local Block_Neutral2 = 10261538677
+local Block_Up       = 10261541266
+local Block_Down     = 10261534663
+
+local Dash_Animation = 10261564805
+local Dash_Attack    = 10261567247
+
+-- Double animations
+
+local DB_Idle        = 3170989102
+local DB_LeftSpin    = 3171520411
+local DB_LS_RSlash   = 3170991372
+local DB_LS_Spin     = 3171464396
+
+-- Dual animations
+
+local Dual_Idle      = 10261542742
+local Dual_Right     = 10261578577
+local Dual_Left      = 10261576689
+
+local Dual_Block     = 10261570305
+local Dual_Block_L   = 10261571663
+local Dual_Block_R   = 10261573087
+
+local Dual_Dash      = 10261574448
+local Dual_Attack    = 10261569252
